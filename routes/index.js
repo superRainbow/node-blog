d58078722920f981842291c75785c29dcf257c9a
吳采洪 <g901612002@gmail.com>
2018-03-04 14:45:07 +0800

    套上樣板

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET post page. */
+router.get('/post', function(req, res, next) {
+  res.render('post', { title: 'Express' });
+});
+
+/* GET archives page. */
+router.get('/dashboard/archives', function(req, res, next) {
+  res.render('dashboard/archives', { title: 'Express' });
+});
+
+/* GET article page. */
+router.get('/dashboard/article', function(req, res, next) {
+  res.render('dashboard/article', { title: 'Express' });
+});
+
+/* GET categories page. */
+router.get('/dashboard/categories', function(req, res, next) {
+  res.render('dashboard/categories', { title: 'Express' });
+});
+
+/* GET signup page. */
+router.get('/dashboard/signup', function(req, res, next) {
+  res.render('dashboard/signup', { title: 'Express' });
 });
 
 module.exports = router;
